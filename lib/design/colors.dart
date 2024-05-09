1d504ad39997757ac6a5142862a42cd8629621a5
Tyler Suehr <tylersuehr@gmail.com>
2024-05-09 18:39:54 -0400

    Custom colors to preview

---
+++ lib/design/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color shamrockGreen = Color(0xFF009D71);
+const Color amaranth = Color(0xFFF01E5B);
+const Color cerulean = Color(0xFF0081A7);
+const Color azul = Color(0xFF016FB9);
+const Color caribbeanCurrent = Color(0xFF197278);
+const Color persianGreen = Color(0xFF1B998B);
